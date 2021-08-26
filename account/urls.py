c3dc752bc7258a6a754109750518436801bd2dfb
Salahuddin Shaikh <33448178+salahoddin88@users.noreply.github.com>
2021-08-26 21:45:39 +0530

    Making html pages dynamics

--- account/urls.py
+++ account/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 """ Url Pattern for root URL Pattern """
 urlpatterns = [
-    path('', views.homePage, name="homePage")    
+    path('', views.homePage, name="homePage"),
+    path('category/<int:category_id>', views.categoryQuestion, name='categoryQuestion'),
+    path('question/<int:question_id>', views.QuestionDetails, name='QuestionDetails')
 ]
--- account/views.py
+++ account/views.py
@@ -1,6 +1,38 @@
 from django.shortcuts import render
+from category.models import Category
+from question_answer.models import Question, Answer
 
 
 """ Home page view function """
 def homePage(request):
-    return render(request, 'home.html')
+    navigation_categories = Category.objects.filter(status=True).order_by('-id')[:5]
+    categories = Category.objects.filter(status=True)
+    questions = Question.objects.filter(status=True).order_by('-id')[:5]
+    return render(request, 'home.html', {
+                                        'navigation_categories':navigation_categories,
+                                        'categories' : categories,
+                                        'questions':questions})
+                                        
+
+def categoryQuestion(request, category_id):
+    navigation_categories = Category.objects.filter(status=True).order_by('-id')[:5]
+    categories = Category.objects.filter(status=True)
+    questions = Question.objects.filter(status=True, category_id=category_id)
+    return render(request, 'category-question.html', {
+        'navigation_categories': navigation_categories,
+        'categories' : categories,
+        'questions' : questions
+    })
+    
+def QuestionDetails(request, question_id):
+    navigation_categories = Category.objects.filter(status=True).order_by('-id')[:5]
+    answers = Answer.objects.filter(question_id=question_id)
+    try:
+        question = Question.objects.get(id=question_id)
+    except Question.DoesNotExist:
+        question = {}
+    return render(request, 'question-details.html', {
+        'navigation_categories': navigation_categories,
+        'question':question,
+        'answers' : answers
+    })
